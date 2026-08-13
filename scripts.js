aef46cdbb7dd343031b1257496496fe73d3dda92
Hott <gabriell.h97@hotmail.com>
2026-08-13 00:27:33 -0300

    added exclusion logic + warning - First delivery version

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@
 const itemInput = document.getElementById("item-input")
 const form = document.getElementById("input-wrapper")
 const itens = document.querySelector("ul")
+const main = document.querySelector("main")
+
 
 // Input Logic + Regex
 
@@ -19,15 +21,68 @@ form.addEventListener("submit",(event) => {
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
   const item = document.createElement("span")
-  const trashIcon = document.createElement("button")
+  const trashButton = document.createElement("button")
 
   checkbox.type = "checkbox"
   item.textContent = itemInput.value
-  trashIcon.id = "delete-button"
+
+  trashButton.type = "button"
+  trashButton.id = "delete-button"
+  trashButton.classList.add("delete-button")
 
   label.append(checkbox,item)
-  newItem.append(label,trashIcon)
+  newItem.append(label,trashButton)
   itens.append(newItem)
   itemInput.value = ""
   itemInput.focus()
 })
+
+// Delete Item Logic
+
+itens.addEventListener("click", (event) =>{
+  const deleteButton = event.target.closest(".delete-button")
+  if (!deleteButton){
+    return
+  }  
+  const elementToDelete = deleteButton.closest("li")
+  elementToDelete.remove()
+  showDeleteMessage()
+  const footer = document.querySelector("footer")
+  footer.addEventListener("click", (event) =>{
+    const deleteCardButton = event.target.closest(".delete-card-button")
+    if (!deleteCardButton){
+      return
+    }  
+    const elementToDelete = deleteCardButton.closest("footer")
+    elementToDelete.remove()
+  })
+})
+
+function showDeleteMessage() {
+  const existingMessage = document.querySelector(".delete-card")
+
+  if (existingMessage) {
+    return
+  }
+
+  const deleteCard = document.createElement("footer")
+  deleteCard.classList.add("grid")
+  deleteCard.classList.add("grid-flow-col")
+
+  const exclamationMark = document.createElement("img")
+  exclamationMark.id = "exclamation-mark"
+  exclamationMark.classList.add("exclamation-mark")
+
+  const deleteSpan = document.createElement("span")
+  deleteSpan.textContent = "O item foi removido da lista"
+
+  const deleteCardButton = document.createElement("button")
+  deleteCardButton.type = "button"
+  deleteCardButton.classList.add("delete-card-button")
+
+  deleteCard.append(exclamationMark,deleteSpan,deleteCardButton)
+  deleteCard.classList.add("delete-card")
+  deleteCard.id = "delete-card"
+  main.append(deleteCard)
+}
+
